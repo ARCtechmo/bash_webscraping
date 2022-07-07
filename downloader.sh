6e58ec73cd0a664b51d3f40523928daa7d289d6c
Rashawn <arctitan78@gmail.com>
2022-07-07 19:18:51 -0400

    added the function to check for the installation of lynx

--- downloader.sh
+++ downloader.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # This is a simple script to download content from the web
 
-
-### NEXT TASK ###
-# - add add the command to download
-# verify installation of xclip and lynx
+# verify installation of xclip
 function install_xclip(){
   echo "-----------TEST: install_xclip(){} func---------------"
-  echo "checking to see if xclip and lynx are installed..."
+  echo "checking to see if xclip is installed..."
   sleep 2
   apt=$(sudo apt-cache policy $1 | awk '/none/ {print $2}')
   if [ "${apt}" == '(none)' ]; then
     echo "$1 is not installed"
     read -p "Would you like to install $1? (Y/N) " ans
+
     case "${ans}" in
       "Y" | "y" | "Yes" | "YES" | "yes")
       echo "------------TEST: YES-------------------"
-      # "------------------code to download goes here---------------------"
+      echo "installing $1...."
+      sleep 2
+      INSTALL=$(sudo apt install $1)
+      echo ${INSTALL}
       ;;
 
       "N" | "n" | "No" | "NO" | "no")
@@ -34,13 +35,50 @@ function install_xclip(){
       ;;
     esac
   else
-    echo "$1 is installed"
+    install_path=$(which $1)
+    echo "$1 is installed in: ${install_path}"
+
   fi
 }
-install_xclip 'xclip'
+# install_xclip 'xclip'
 
 # verify installation of lynx
 function install_lynx(){
   echo "-----------TEST: install_lynx(){} func---------------"
+  echo "checking to see if lynx is installed..."
+  sleep 2
+  apt=$(sudo apt-cache policy $1 | awk '/none/ {print $2}')
+  if [ ${apt} == '(none)' ]; then
+    echo "$1 is not installed."
+    read -p "Would you like to install $1? (Y/N) " ans
 
+    case "${ans}" in
+      "Y" | "y" | "Yes" | "YES" | "yes")
+      echo "------------TEST: YES-------------------"
+      echo "installing $1...."
+      sleep 2
+      INSTALL=$(sudo apt install $1)
+      echo ${INSTALL}
+      ;;
+
+      "N" | "n" | "No" | "NO" | "no")
+      echo "You need to install $1 to continue."
+      echo "exiting..."
+      sleep 2
+      exit
+      ;;
+
+      *)
+      echo "------------TEST: invalid input---------------"
+      echo "exiting..."
+      sleep 2
+      exit
+      ;;
+    esac
+
+  else
+    install_path=$(which $1)
+    echo "$1 is installed in: ${install_path}"
+  fi
 }
+install_lynx 'lynx'
